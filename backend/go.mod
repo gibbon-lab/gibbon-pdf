f518a689cb7add79e05b6c728e254b6abce71fed
ItsAlex <me@itsalex.fr>
2023-04-22 19:14:41 +0900

    --wip-- [skip ci]

--- backend/go.mod
+++ backend/go.mod
@@ -30,6 +30,9 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
--- backend/internal/api/main.go
+++ backend/internal/api/main.go
@@ -1,16 +1,20 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	"github.com/xeipuuv/gojsonschema"
 )
 
 var engine *gin.Engine
 
-const templatePath = "../pdf-templates/"
+const templatePath = "/templates/"
 
 func InitApi() {
 	engine = gin.Default()
@@ -43,8 +47,66 @@ func InitApi() {
 			return
 		}
 
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"templates": dirs,
+		})
+	})
+
+	engine.GET("/v1/templates/:templateName", func(c *gin.Context) {
+		jsonSchemaFilePath := fmt.Sprintf(
+			"%s%s/schema.json",
+			templatePath,
+			c.Param("templateName"),
+		)
+		fileBytes, err := ioutil.ReadFile(jsonSchemaFilePath)
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to read file"})
+			return
+		}
+
+		var data interface{}
+		err = json.Unmarshal(fileBytes, &data)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to parse JSON"})
+			return
+		}
+
+		schemaLoader := gojsonschema.NewReferenceLoader(fmt.Sprintf("file://%s", jsonSchemaFilePath))
+		m := map[string]interface{}{"type": "string"}
+		documentLoader := gojsonschema.NewGoLoader(m)
+		result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Bad body"})
+			return
+		}
+
+		if result.Valid() {
+			fmt.Printf("The document is valid\n")
+		} else {
+			fmt.Printf("The document is not valid. see errors :\n")
+			for _, err := range result.Errors() {
+				// Err implements the ResultError interface
+				fmt.Printf("- %s\n", err)
+			}
+		}
+
+		c.JSON(http.StatusOK, data)
+	})
+
+	engine.POST("/v1/templates/:templateName/html", func(c *gin.Context) {
+		// show rendered html
+
+		c.JSON(http.StatusOK, gin.H{
+			// "json_schema": ,
+		})
+	})
+
+	engine.POST("/v1/templates/:templateName/pdf", func(c *gin.Context) {
+		// show rendered pdf
+
+		c.JSON(http.StatusOK, gin.H{
+			// "json_schema": ,
 		})
 	})
 
